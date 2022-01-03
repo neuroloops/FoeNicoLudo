5da1dae02c1e6ac7d0870e71f825de4a39b156a4
ludo loops <ludoloops@protonmail.com>
2022-01-03 16:25:56 +0100

    Feat: featch data airtable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,34 @@
+require('dotenv').config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "NicoLudoFOE",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'NicoLudoFOE',
   },
-  plugins: ["gatsby-plugin-emotion"],
-};
+  plugins: [
+    'gatsby-plugin-emotion',
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.GATSBY_AIRTABLE_API,
+        concurrency: 5,
+        tables: [
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: `Table`,
+            tableView: `Grid view`,
+          },
+        ],
+        tables: [
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: `Table`,
+            tableView: `Nico`,
+          },
+        ],
+      },
+    },
+  ],
+}
